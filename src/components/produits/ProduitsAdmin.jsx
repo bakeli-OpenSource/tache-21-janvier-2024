d3e84cd8b6f28119b6f3eb4596dac88d26626138
AwaNdioneWade <wadendioneawa@gmail.com>
2024-02-06 16:45:41 +0000

    formulaire ajout

--- src/components/produits/ProduitsAdmin.jsx
+++ src/components/produits/ProduitsAdmin.jsx
@@ -10,9 +10,48 @@ const ProduitsAdmin = () => {
 
   const {table, table2, actions} = useProduits();
   const {open} = useSidebare()
+  
+  const inputs = [
+    {
+      label: "Nom produit",
+      type: "text"
+    },
+    {
+      label: "Image catégorie",
+      type: 'file'
+    },
+    {
+      label: "Nombre produit",
+      type: 'number'
+    },
+    {
+      label: "Staut catégorie",
+      type: 'text'
+    }
+  ]
+  
+  const selects = [
+    {
+      label: 'Catégorie',
+      options: [
+        'categorie1',
+        'categorie2',
+        'categorie3',
+      ]
+    }
+  ]
+
+  const textarea = {
+    description: 'description'
+  }
+
   return (
     <div className={`${open ? "md:ml-[225px]" : "md:ml-[85px]"  } m-4 `}>
-      <HeaderTable title="Produits" nomAjout="Ajouter des produits" body={<Formulaire />} />
+      <HeaderTable
+       title="Produits"
+       nomAjout="Ajouter des produits" 
+       body={<Formulaire inputs={inputs} selects={selects} textarea={textarea} />} 
+       />
       <Table thead={table} tbody={table2} actions={actions} />
     </div>
   )
