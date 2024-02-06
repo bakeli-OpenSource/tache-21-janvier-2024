f8fa46135d164dfcc7c2a9a97da1428fa2315c08
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2024-02-06 16:42:58 +0000

    dossier users

--- src/pages/utilisateurs/Accueil.jsx
+++ src/pages/utilisateurs/Accueil.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Footer from '../../usersComponents/footer/Footer'
 
 export default function Accueil() {
   return (
     <div>
         <h1>Accueil</h1>
+
+        <footer>
+          <Footer/>
+        </footer>
     </div>
   )
 }
--- src/usersComponents/footer/Footer.jsx
+++ src/usersComponents/footer/Footer.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+      <div className='container flex justify-between mt-96'>
+        <div className=' flex mx-5 gap-x-8'>
+            <div>
+                <h4 className='text-base font-bold italic my-6'>CONTACT US</h4>
+                <div>+221 77 982 54 32</div>
+                <div>kaysolu@gmail.com</div>
+                <div>Dakar-bakeli</div>
+            </div>
+            <div>
+                <h4 className='text-base font-bold italic my-6'>CUSTOMERS</h4>
+                <p>Start a Return</p>
+                <p>Return Policy</p>
+                <p>FAQ</p>
+                <p>Catalogs and Mailers</p>
+                <p>About Group Gifting</p>
+            </div>
+            <div>
+                <h4 className='text-base font-bold italic my-6'>COMPANY</h4>
+                <p>About Us</p>
+                <p>Sustainability</p>
+                <p>Discover Revive</p>
+                <p>Careers</p>
+                <p>Privacy Policy</p>
+                <p>Terms</p>
+            </div>
+        </div>
+        <div className=''>
+            <h3 className='text-lg'>Get the latest new from us</h3>
+            <input className="border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500"/>
+            <p>By signing up, you agree to our Privavy Police and Terms of Services</p>
+            <button className='bg-black text-white'>Sign Up</button>
+        </div>
+      </div>
   )
 }
 
