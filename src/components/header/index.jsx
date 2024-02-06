d177f2a4e142c0eb579a7f4fa912ef543bda72de
Awa_la_princesse <miss-ndeya>
2024-02-06 18:59:01 +0000

    ajout profile

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
-import { FaSquareEnvelope } from "react-icons/fa6";
-import { IoIosNotifications } from "react-icons/io";
 import useSidebare from "../../utils/hooks/useSidebare";
 import UserDropdown from "../user/UserDropdown";
+import Notification from "../user/Notification";
+import Message from "../user/Message";
 
 const Header = () => {
   const { toggleSidebare } = useSidebare();
+
+  const items = ['Premier', 'Deuxième', 'Troisième '];
 
   return (
     <header className="z-[10] bg-gray-800 fixed w-full m-0 flex justify-between text-gray-100 w-100 px-5 py-3">
@@ -17,17 +19,22 @@ const Header = () => {
       />
       <div className="flex gap-1 justify-around align-center">
         <div className="  hover:bg-slate-50 p-1.5 hover:rounded-md hover:text-gray-700">
-          <FaSquareEnvelope className="cursor-pointer p-" size={20} />
+          <Message />
         </div>
         <div className="  hover:bg-slate-50 p-1.5 hover:rounded-md hover:text-gray-700">
-          <IoIosNotifications className="cursor-pointer p-" size={20} />
+          <Notification />
         </div>
         <div className="  hover:bg-slate-50 p-1.5 hover:rounded-md hover:text-gray-700">
           <UserDropdown />
         </div>
+        
       </div>
     </header>
   );
 };
 
 export default Header;
+
+
+
+
