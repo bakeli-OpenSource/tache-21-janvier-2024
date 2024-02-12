1efe4e80044bea39ff34f6865094c8b886201f59
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-02-12 08:40:41 +0000

    connexon

--- src/pages/admin/connexion/AdminConnexion.jsx
+++ src/pages/admin/connexion/AdminConnexion.jsx
@@ -23,7 +23,8 @@ function SignUp() {
   const onSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch('http://localhost:4000/api/user');
-    console.log(response)
+    const data = await response.json()
+    console.log(data)
     if (response.ok) {
       navigate("/check-your-email");
     } else {
