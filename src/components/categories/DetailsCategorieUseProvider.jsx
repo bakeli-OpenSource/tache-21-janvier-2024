58dd719d4ad2942db9c1cc3a7fc1dc62cd79813c
Ladji Timera <ladjitimera52@gmail.com>
2024-02-17 16:30:18 +0000

    Navbar Utilisateur

--- src/components/categories/DetailsCategorieUseProvider.jsx
+++ src/components/categories/DetailsCategorieUseProvider.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ProduitContextProvider from '../../utils/contexte/ProduitsContext'
 import DetailsCategorie from '../../pages/admin/DetailsCategorie'
+import Graphique2 from '../../pages/admin/graphique2/Graph'
+
 
 const DetailsCategorieUseProvider = () => {
   return (
     <ProduitContextProvider>
+      <Graphique2/>
       <DetailsCategorie />
     </ProduitContextProvider>
   )
--- src/pages/admin/graphique2/Graph.jsx
+++ src/pages/admin/graphique2/Graph.jsx
@@ -53,3 +53,4 @@ export default Graphique2;
 
 
 
+
