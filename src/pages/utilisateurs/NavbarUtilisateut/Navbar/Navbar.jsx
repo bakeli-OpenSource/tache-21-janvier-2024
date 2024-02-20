e190a9083be68192fa6e67357be3c6163e90af46
Awa_la_princesse <miss-ndeya>
2024-02-20 16:30:04 +0000

    ajout utilisateur

--- src/pages/utilisateurs/NavbarUtilisateut/Navbar/Navbar.jsx
+++ src/pages/utilisateurs/NavbarUtilisateut/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { MdMenu, MdClose } from "react-icons/md";
 import { BsPersonCircle, BsSearch } from "react-icons/bs";
 import { ShoppingCartIcon } from "@heroicons/react/outline";
 import NavInput from "../NavInput";
-import Dropdown from "../../../../usersComponents/userConnexion/Dropdown";
+import Dropdown from "../../../../usersComponents/userConnexion/dropdown";
 
 const Navbar = ({className}) => {
   const [open, setOpen] = useState(false);
--- src/usersComponents/userConnexion/Dropdown.jsx
+++ src/usersComponents/userConnexion/Dropdown.jsx
@@ -1,43 +1,74 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { FaRegUser, FaRegHeart } from "react-icons/fa";
 import { DiGhostSmall } from "react-icons/di";
+import useGlobal from "../../utils/hooks/useGlobal";
+import { prenom } from "../../utils/contexte/GlobalContext";
 
 const Dropdown = () => {
-  const [dropdown, setDropdown] = useState(false);
+  const { handleLogoutUser, profileUser, dropdown, handleToggle } = useGlobal();
 
-  const handleToggle = () => {
-    setDropdown(!dropdown);
+  const navigate = useNavigate();
+  // const [dropdown, setDropdown] = useState(false);
+
+  // const handleToggle = () => {
+  //   setDropdown(!dropdown);
+  // };
+
+  useEffect(() => {
+    profileUser()
+  }, [])
+
+  const deconnexion = () => {
+    handleToggle();
+
+    if (tokenClient === null) {
+      navigate("/connexion");
+    } else {
+      handleLogoutUser();
+    }
   };
 
   const tokenClient = localStorage.getItem("tokenclient");
 
-  console.log(tokenClient);
-
   return (
     <div className="relative">
       <div
-        className="font-medium flex items-center gap-"
+        className="font-medium cursor-pointer flex items-center gap-"
         onClick={handleToggle}
       >
         <FaRegUser className="cursor-pointer mr-2" size={20} />
-        Se connecter
+        
+        {tokenClient === null ? "Se connecter" : `bonjour ${prenom}` }
         <RiArrowDropDownLine className="p-0 mt-2 mx-0" size={30} />
       </div>
 
       {dropdown && (
-        <div className={ `absolute  right-0 mt- w-48  flex ${tokenClient === null ? " flex-col" : "flex-col-reverse mt-5" }    bg-white px-  border border-gray-200 rounded-md shadow-lg py-4`}>
-          <Link
-            to="/connexion"
-            onClick={handleToggle}
-            className={`block ${tokenClient === null ? " " : "border-t-2 uppercase border-slate-700"} px-4 mb-4 shadow-md py-2 mx-3 text-center text-upercase text-sm bg-gray-800 text-white rounded hover:bg-gray-900`}
+        <div
+          className={`absolute  right-0 mt- w-48  flex ${
+            tokenClient === null ? " flex-col" : "flex-col-reverse mt-5"
+          }    bg-white px-  border border-gray-200 rounded-md shadow-lg py-4`}
+        >
+          <button
+            onClick={deconnexion}
+            className={`block ${
+              tokenClient === null
+                ? " "
+                : "border-t-2 uppercase border-slate-700"
+            } px-4 mb-4 shadow-md py-2 mx-3 text-center text-upercase text-sm bg-gray-800 text-white rounded hover:bg-gray-900`}
           >
-            {tokenClient === null ? "se connecter"  : "deconnexion"}
-          </Link>
-          <div className={`${tokenClient ? "border-b-2 border-slate-700 mb-3" : "border-t-2 border-slate-700"}  flex flex-col`}>
+            {tokenClient === null ? "se connecter" : "deconnexion"}
+          </button>
+          <div
+            className={`${
+              tokenClient
+                ? "border-b-2 border-slate-700 mb-3"
+                : "border-t-2 border-slate-700"
+            }  flex flex-col`}
+          >
             <Link
-              to="/connexion"
+              to="/"
               onClick={handleToggle}
               className=" px-4 my-0  py-2 flex items-center gap-3   text-upercase text-sm hover:bg-gray-200 "
             >
