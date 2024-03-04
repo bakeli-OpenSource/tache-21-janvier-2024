37e448b86cda11da52fd3244491640a24b169372
Ladji Timera <ladjitimera52@gmail.com>
2024-03-04 12:45:51 +0000

    recupérer

--- src/pages/utilisateurs/NavbarUtilisateut/NavbarLinks/Arrivals.jsx
+++ src/pages/utilisateurs/NavbarUtilisateut/NavbarLinks/Arrivals.jsx
@@ -1,92 +1,3 @@
-// import React, { useContext, useEffect, useState } from "react";
-// import { ProduitsContext } from "../../../../utils/contexte/ProduitsContext";
-// import useGlobal from "../../../../utils/hooks/useGlobal";
-// import Loader from "../../../../components/loader/loader";
-// import { usePanier } from "../../../../utils/contexte/PanierContext";
-// import CardProduit from "../../../../usersComponents/cards/CardProduit";
-// import axiosInstance from "../../../../utils/axiosInstance";
-
-
-// const Arrivals = () => {
-//   const { produits } = useContext(ProduitsContext);
-//   const [filteredProducts, setFilteredProducts] = useState([]);
-//   const { addToCart } = usePanier();
-//   const [produitAimer, setProduitAimer] = useState(() => {
-//     const listesEnvies = localStorage.getItem("produitAimer");
-//     return listesEnvies ? JSON.parse(listesEnvies) : [];
-//   });
-//   const { setDropdown } = useGlobal();
-//   useEffect(() => {
-//     const fetchNewArrivals = async () => {
-//       try {
-//         const response = await axiosInstance.get(
-//           "/produits"
-//         );
-//         setFilteredProducts(response.data);
-//       } catch (error) {
-//         console.error(
-//           "Erreur lors de la récupération des nouveaux arrivages:",
-//           error
-//         );
-//       }
-//     };
-
-//     fetchNewArrivals();
-//   }, []);
-
-//   useEffect(() => {
-//     const sortProductsByDate = () => {
-//       const sortedProducts = produits.sort((a, b) => {
-//         return new Date(b.date) - new Date(a.date);
-//       });
-  
-//       const latestProducts = sortedProducts.slice(0, 10).reverse(); 
-//       setFilteredProducts(latestProducts);
-//     };
-  
-//     // if (produits.length > 0) {
-//       sortProductsByDate();
-      
-//     // }
-
-
-//     console.log(sortProductsByDate);
-//   }, [produits]);
-
-  
-
-//   useEffect(() => {
-//     localStorage.setItem("produitAimer", JSON.stringify(produitAimer));
-//   }, [produitAimer]);
-
-//   return (
-//     <div
-//     data-aos="zoom-in"
-//       onClick={() => setDropdown(false)}
-//       className="px-9  mt-[50px] mx-auto z-0 flex flex-col  "
-//     >
-
-//       <div className="mt-[30px] ">
-//         {produits.length > 0 ? (
-//           <>
-//             <h1 className="mb-5">New Arrivals</h1>
-
-//             <div className="grid  grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-[30px]  mx-auto md:max-w-none md:mx-0">
-//               {filteredProducts.map((produit) => (
-//                 <CardProduit produit={produit} key={produit._id} />
-//               ))}
-//             </div>
-//           </>
-//         ) : (
-//           <Loader />
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-// export default Arrivals;
-
-
 import React, { useContext, useEffect, useState } from "react";
 import { ProduitsContext } from "../../../../utils/contexte/ProduitsContext";
 import useGlobal from "../../../../utils/hooks/useGlobal";
@@ -123,22 +34,23 @@ const Arrivals = () => {
     fetchNewArrivals();
   }, []);
 
+  const sortProductsByDate = () => {
+    const sortedProducts = produits.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+
+    const latestProducts = sortedProducts.slice(0, 10).reverse(); 
+    setFilteredProducts(latestProducts);
+  };
+
   useEffect(() => {
-    const sortProductsByDate = () => {
-      const sortedProducts = produits.sort((a, b) => {
-        // Triez les produits du plus récent au plus ancien en fonction de la date d'ajout
-        return new Date(b.dateAjout) - new Date(a.dateAjout);
-      });
-
-      // Sélectionnez les 10 premiers produits après le tri
-      const latestProducts = sortedProducts.slice(0, 10);
-      setFilteredProducts(latestProducts);
-    };
-
-    if (produits.length > 0) {
+    
       sortProductsByDate();
-    }
+    
+    console.log(sortProductsByDate);
   }, []);
+
+  
 
   useEffect(() => {
     localStorage.setItem("produitAimer", JSON.stringify(produitAimer));
@@ -150,6 +62,7 @@ const Arrivals = () => {
       onClick={() => setDropdown(false)}
       className="px-9  mt-[50px] mx-auto z-0 flex flex-col  "
     >
+
       <div className="mt-[30px] ">
         {produits.length > 0 ? (
           <>
@@ -169,3 +82,5 @@ const Arrivals = () => {
   );
 };
 export default Arrivals;
+
+
--- src/pages/utilisateurs/NavbarUtilisateut/NavbarLinks/Shop.jsx
+++ src/pages/utilisateurs/NavbarUtilisateut/NavbarLinks/Shop.jsx
@@ -86,34 +86,43 @@ const Shop = () => {
 
   const { setDropdown } = useGlobal();
 
-  // // Fonction pour mettre à jour l'état de survol pour un produit spécifique
-  // const handleHoverEtoils = (index, etoils) => {
-  //   let newHoverArray = [...hoverArray]; // Copie de l'array d'états actuel
-  //   newHoverArray[index] = etoils; // Met à jour l'état de survol pour le produit à l'index spécifié
-  //   setHoverArray(newHoverArray); // Met à jour l'état du composant
-  // };
-
   return (
     <>
-         <div
-    data-aos="zoom-in"
-      onClick={() => setDropdown(false)}
-      className="px-9  mt-[50px] mx-auto z-0 flex flex-col  "
-    >
-      <div className="mt-[30px] ">
-        
-          <>
-            <h1 className="mb-5">New Arrivals</h1>
+      <div
+        data-aos="zoom-in"
+        onClick={() => setDropdown(false)}
+        className="  mt-[50px] mx-auto z-0 flex flex-col  "
+      >
+        <div className="flex px-9 flex-wrap mt-10">
+          <h1 className=" text-2xl mr-5">Shop</h1>
+          <ComponentButton
+            className="hover:bg-gray-200 text-black ml-6 w-auto px-3 py-2 text-md tracking-widest rounded"
+            texte={"Tous les Produits"}
+            onClick={() => handleChange()}
+          />
+          {categories.map((categorie, index) => (
+            <div className="flex" key={index}>
+              <ComponentButton
+                className=" focus:bg-gray-200 active:bg-gray-200 hover:bg-gray-200  text-black ml-6 w-auto px-3 py-2 text-md tracking-widest rounded"
+                texte={categorie.nom}
+                onClick={() => handleChange(categorie.nom)}
+              />
+            </div>
+          ))}
+        </div>
 
-            <div className="grid  grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-[30px]  mx-auto md:max-w-none md:mx-0">
+        <div>
+          {filteredProducts.length > 0 ? (
+            <div className="grid px-6 md:ps-9 md:pe-9  grid-cols-1 mt-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-[30px]  mx-auto md:max-w-none md:mx-0">
               {filteredProducts.map((produit) => (
                 <CardProduit produit={produit} key={produit._id} />
               ))}
             </div>
-          </>
-        
+          ) : (
+            <Loader />
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
