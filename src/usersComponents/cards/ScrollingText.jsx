12f3e3eadf16df1dbeb9fd729e90cbbda9c1fb52
Awa_la_princesse <miss-ndeya>
2024-03-05 17:05:56 +0000

    ajout

--- src/usersComponents/cards/ScrollingText.jsx
+++ src/usersComponents/cards/ScrollingText.jsx
@@ -3,8 +3,8 @@ import Client from '../../assets/images/client.jpg'
 
 const ScrollingText = () => {
   return (
-    <div className="min-h-[550px] flex justify-center items-center py-12 sm:py-0">
-      <div className="container bg-white">
+    <div className="min-h-[550px]  flex justify-center items-center py-12 sm:py-0">
+      <div className="container py-6 px-6 bg-white">
         <h2 className="text-2xl font-medium mb-4 text-center">L’Art de Faire Moins de Choix, Mais de Meilleurs Choix</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">   
           <div className="whitespace-wrap px-12 sm:px-0"  data-aos="zoom-out">
@@ -17,7 +17,7 @@ const ScrollingText = () => {
             </div>
           </div>
        
-          <div data-aos="zoom-in" className='rounded pb-2 bg-gray-200'>
+          <div data-aos="zoom-in" className='rounded pt-2 pb-2 bg-gray-200'>
             <img
               src={Client}
               alt="public"
