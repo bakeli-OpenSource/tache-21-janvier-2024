faba86777060c8faf2ebd3fffd299ba7c35ab018
Santos25 <abdoulayejuniorsantos@gmail.com>
2024-03-07 16:43:37 +0100

    recuperation

--- src/components/commandes/DetailsCommande.jsx
+++ src/components/commandes/DetailsCommande.jsx
@@ -17,26 +17,43 @@ const DetailsCommande = () => {
       className={`${open ? "md:ml-[225px]" : "md:ml-[85px]"} m-4 `}
     >
       <HeaderTable title="Détails Commandes" />
-      <div className="flex shadow-lg rounded-md bg-white justify-between border p-4 py-6 mt-5 gap-">
+      <div className=" shadow-lg rounded-md bg-white justify-between border p-4 py-6 mt-5 gap-">
         <div className="mt-5">
           <p className="font-medium">
-              Nom Complete : {data.nom}  {data.prenom}
+              Nom Complete : <span className="font-normal mx-auto">{data.nom}  {data.prenom}</span>
           </p>
-          <p className="font-medium">Email : {data.email}</p>
-          <p className="font-medium">Telephone : {data.telephone}</p>
-          <p className="font-medium">Date : {data.date}</p>
+          <p className="font-medium">Email : <span className="font-normal mx-auto">{data.email}</span></p>
+          <p className="font-medium">Telephone : <span className="font-normal mx-auto">{data.telephone}</span></p>
+          <p className="font-medium">Date : <span className="font-normal mx-auto">{data.date}</span></p>
           <p className="font-medium">
-            Prix livraison : {data.prixLivraison}cfa
+            Prix livraison : <span className="font-normal mx-auto">{data.prixLivraison}</span>cfa
           </p>
-          <p className="font-medium">Prix total : {data.prixTotal}cfa</p>
+          <p className="font-medium">Prix total : <span className="font-normal mx-auto">{data.prixTotal}cfa</span></p>
+          <div>
+          <button
+              className={`px-2 my-3 py-1 rounded text-white ${
+                data.etat === "en cours"
+                  ? "bg-orange-500"
+                  : data.etat === "en livraison"
+                  ? "bg-yellow-500"
+                  : data.etat === "livrée"
+                  ? "bg-green-500"
+                  : data.etat === "en attente"
+                  ? "bg-red-600"
+                  : ""
+              }`}
+              >
+              {data.etat}
+              </button>
+          </div>
         </div>
-      </div>
+      
 
       <div className='flex'>
       {data?.produit?.map((produit, index) => (
         // <div className="flex shadow-lg rounded-md bg-white justify-between    gap-">
-          <div className="flex-shrink-0 w-full md:w-[25rem] lg:w-1/3 xl:w-96 mb-5 p-4 py-6 mt-5">
-          <div key={index} className="card min-w-full flex items-center p-5 mb-4 bg-white rounded-md shadow-md gap-5">
+          <div className="flex-shrink-0 mx-auto md:w-[25rem] lg:w-1/3 xl:w-96 mb-5 mt-5">
+          <div key={index} className="card min-w-full flex items-center p-5 mb-4 bg-gray-100 rounded-lg shadow-lg border-s-4 border-gray-800 gap-5">
             <div className="w-16">
               <img
                 className="min-w-full h-auto"
@@ -55,29 +72,14 @@ const DetailsCommande = () => {
                 <p>
                   Prix total produit : {data.prixProduit[index]}cfa
                 </p>
-
-                <button
-
-                className={`px-2 my-3 py-1 rounded text-white ${
-                  data.etat === "en cours"
-                    ? "bg-orange-500"
-                    : data.etat === "en livraison"
-                    ? "bg-yellow-500"
-                    : data.etat === "livrée"
-                    ? "bg-green-500"
-                    : data.etat === "en attente"
-                    ? "bg-red-600"
-                    : ""
-                }`}
-              >
-                {data.etat}
-              </button>
+                
               </div>
             </div>
           </div>
         </div>
       ))}
       </div>
+    </div>
     </div>
   );
 };
